468b73f4aed42f75c25e46fd47a736f99e0dd52b
Martin Jacobson <helmasur.king@protonmail.com>
2021-02-27 09:01:15 +0100

    resize on image load

--- mirror-finder.py
+++ mirror-finder.py
@@ -20,41 +20,62 @@ def load_image(filename):
         print ('Cannot load image:', fullpath)
         raise SystemExit(str(geterror()))
     image = image.convert()
-    return image, image.get_rect()    
+    return image, image.get_rect()
+
+def load_n_resize(filename):
+    fullpath = os.path.join(data_dir, filename)
+    dispsurf = pygame.display.get_surface()
+    disprect = dispsurf.get_rect()
+    try:
+        image = pygame.image.load(fullpath)
+    except pygame.error:
+        print ('Cannot load image:', fullpath)
+        raise SystemExit(str(geterror()))
+    image = image.convert()
+    imagerect = image.get_rect()
+    scale = min(1.0 * disprect.height / imagerect.height, 1.0 * disprect.width/2.0 / imagerect.width)
+    width = int(scale * imagerect.width)
+    height = int(scale * imagerect.height)
+    Limage = pygame.Surface((width,height),0,image)
+    pygame.transform.scale(image, (width, height), Limage)
+    Rimage = pygame.transform.flip(Limage, True, False)
+    return Limage, Rimage
 
 class Bild(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self) #call Sprite initializer
-        self.image, self.rect = load_image('test.jpg')
-        self.crop = self.rect.copy()
-        self.mirrorrect = self.rect.copy()
-        self.mirrorcrop = self.rect.copy()
-        self.mirrorimage = pygame.transform.flip(self.image, True, False)
-        self.rect.centery = (240)
-        self.mirrorrect.centery = (240)
+        self.Limage, self.Rimage = load_n_resize('test.jpg')
+        self.Lrect = self.Limage.get_rect()
+        self.Rrect = self.Rimage.get_rect()
+        self.Lcrop = self.Lrect.copy()
+        self.Rcrop = self.Rrect.copy()
+        #self.mirrorrect = self.rect.copy()
+        #self.mirrorcrop = self.rect.copy()
+        #self.mirrorimage = pygame.transform.flip(self.image, True, False)
+        
 
     def update(self):
         mouse = pygame.mouse.get_pos()
-        crop = min(mouse[0], self.rect.width)
-        self.crop.width = crop
-        self.mirrorrect.left = crop
-        self.mirrorcrop.width = crop
-        self.mirrorcrop.left = self.rect.width-crop
+        crop = min(mouse[0], self.Lrect.width)
+        self.Lcrop.width = crop
+        self.Rrect.left = crop
+        self.Rcrop.width = crop
+        self.Rcrop.left = self.Lrect.width-crop
 
-    def resize(self, displaywidth):
-        #ratio = self.rect.height/self.rect.width
-        factor = (displaywidth/2.0) / self.rect.width
-        width = int(self.rect.width * factor)
-        height = int(self.rect.height * factor)
-        self.rect.width = width
-        self.rect.height = height
-        self.mirrorrect.width = width
-        self.mirrorrect.height = height
-        self.crop.height = height
-        self.mirrorcrop.height = height
-        self.image = pygame.transform.scale(self.image, (width, height))
-        
-
+    # def resize(self, displaywidth, displayheight):
+    #     factor = (displaywidth/2.0) / self.rect.width
+    #     width = int(self.rect.width * factor)
+    #     height = int(self.rect.height * factor)
+    #     self.rect.width = width
+    #     self.rect.height = height
+    #     self.mirrorrect.width = width
+    #     self.mirrorrect.height = height
+    #     self.crop.height = height
+    #     self.mirrorcrop.height = height
+    #     self.image = pygame.transform.scale(self.image, (width, height))
+    #     self.mirrorimage = pygame.transform.scale(self.mirrorimage, (width, height))
+    #     self.rect.centery = (displayheight/2)
+    #     self.mirrorrect.centery = (displayheight/2)
 
         
 class Bild2(pygame.sprite.Sprite):
@@ -80,6 +101,7 @@ def main():
     #bild2 = Bild2()
 
     allsprites = pygame.sprite.Group(bild) #lägger till spriten 'bild' i gruppen allsprites
+    #bild.resize(640,480)
 
 
     #mainloop
@@ -95,19 +117,23 @@ def main():
             elif event.type == KEYDOWN and event.key == K_f:
                 if fullscreen:
                     fullscreen = False
-                    bild.resize(640)
+                    #bild.resize(640,480)
                     pygame.display.set_mode((640,480))
+                    bild = Bild()
+                    allsprites = pygame.sprite.Group(bild)
                 else:
                     fullscreen = True
                     pygame.display.set_mode((1680,1050), pygame.FULLSCREEN)
-                    bild.resize(1680)
+                    bild = Bild()
+                    allsprites = pygame.sprite.Group(bild)
+                    #bild.resize(1680,1050)
 
             
     
         allsprites.update()
         screen.fill ((50, 0, 0))
-        screen.blit(bild.image, bild.rect, bild.crop)
-        screen.blit(bild.mirrorimage, bild.mirrorrect, bild.mirrorcrop)
+        screen.blit(bild.Limage, bild.Lrect, bild.Lcrop)
+        screen.blit(bild.Rimage, bild.Rrect, bild.Rcrop)
         #allsprites.draw(screen)
         pygame.display.flip()
 
