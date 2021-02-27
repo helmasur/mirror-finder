15033573be1dcfe1f65ddd3a74051c966dbab281
Martin Jacobson <helmasur.king@protonmail.com>
2021-02-27 08:37:52 +0100

    first commit

---
+++ mirror-finder.py
@@ -0,0 +1,70 @@
+﻿# coding: utf-8
+import os, pygame
+import sys
+import time
+from pygame.locals import *
+import random
+from math import *
+import colorsys
+from PIL import Image
+import numpy
+
+main_dir = os.path.split(os.path.abspath(__file__))[0]
+data_dir = os.path.join(main_dir, 'data')
+
+def load_image(filename):
+    fullpath = os.path.join(data_dir, filename)
+    try:
+        image = pygame.image.load(fullpath)
+    except pygame.error:
+        print ('Cannot load image:', fullpath)
+        raise SystemExit(str(geterror()))
+    image = image.convert()
+    return image, image.get_rect()    
+
+class Bild(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self) #call Sprite initializer
+        self.image, self.rect = load_image('test.jpg')
+        
+        
+        
+
+
+
+def main():
+
+    pygame.init()
+    screen = pygame.display.set_mode((640, 480), 0, 32)
+    
+    bild = Bild()
+
+    allsprites = pygame.sprite.Group(bild) #lägger till spriten 'bild' i gruppen allsprites
+
+       
+    allsprites.draw(screen)
+    pygame.display.flip()
+    allsprites.draw(screen)
+    pygame.display.flip()    
+
+    
+
+    #mainloop
+    going=True
+    while going:
+
+        #Handle Input Events
+        for event in pygame.event.get():        
+            if event.type == QUIT:
+                going = False
+            elif event.type == KEYDOWN and event.key == K_ESCAPE:
+                going = False
+            elif event.type == KEYDOWN:
+                going = False                #plot.setLevel(int(event.unicode))
+    
+        allsprites.update()
+        allsprites.draw(screen)
+        pygame.display.flip()
+
+if __name__ == '__main__':
+    main()
