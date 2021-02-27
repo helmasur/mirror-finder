fcb1ec338f0492f30fd5fc8c8dd48f0d10a6d3fb
Martin Jacobson <helmasur.king@protonmail.com>
2021-02-27 09:44:31 +0100

    fix crop and rotation interaction

--- mirror-finder.py
+++ mirror-finder.py
@@ -22,10 +22,15 @@ def load_image(filename):
     image = image.convert()
     return image
 
+def save_image(surface, filename):
+    fullpath = os.path.join(data_dir, filename)
+    pygame.image.save(surface, fullpath)
+
 class Bild(pygame.sprite.Sprite):
     def __init__(self, dispsurf):
         pygame.sprite.Sprite.__init__(self) #call Sprite initializer
-        self.original = load_image('stor.tif')
+        self.filename = 'kant.jpg'
+        self.original = load_image(self.filename)
         self.image = self.original.copy()
         self.rect = self.image.get_rect()
         self.disprect = dispsurf.get_rect()
@@ -44,6 +49,16 @@ class Bild(pygame.sprite.Sprite):
         self.Lrect.centery = self.disprect.height / 2
         self.Rrect.centery = self.disprect.height / 2
 
+        self.croppos = 0
+
+        self.rotate90 = False
+        self.rotateString = '0deg'
+        self.rightSideMirror = True
+        self.mirrorString = 'Rmirror'
+        self.verticalFlip = False
+        self.flipString = '0flip'
+
+
     def resize(self, dispsurf):
         self.disprect = dispsurf.get_rect()
         scale = min(1.0 * self.disprect.height / self.rect.height, 1.0 * self.disprect.width/2.0 / self.rect.width)
@@ -61,28 +76,48 @@ class Bild(pygame.sprite.Sprite):
         self.Lrect.centery = self.disprect.height / 2
         self.Rrect.centery = self.disprect.height / 2
 
-    def rotate(self, angle):
-        self.image = pygame.transform.rotate(self.original, angle)
-        self.rect = self.image.get_rect()
+    def orientation(self):
+        if self.rotate90:
+            self.image = self.original
+            self.rect = self.image.get_rect()
+        else:
+            self.image = pygame.transform.rotate(self.original, 270)
+            self.rect = self.image.get_rect()
+            
+        self.rotate90 = self.rotate90 == False            #ändra boolvärdet
 
-    def flip(self, isHorizontal):
-        if isHorizontal:
+    def flip(self, direction):
+        if direction == 'horizontal':
+            self.rightSideMirror = self.rightSideMirror == False
             self.Limage = pygame.transform.flip(self.Limage, True, False)
             self.Rimage = pygame.transform.flip(self.Rimage, True, False)
-        else:
+        elif direction == 'vertical':
+            self.verticalFlip = self.verticalFlip == False
             self.Limage = pygame.transform.flip(self.Limage, False, True)
             self.Rimage = pygame.transform.flip(self.Rimage, False, True)
 
     def update(self):
         mouse = pygame.mouse.get_pos()
-        crop = min(mouse[0]/2, self.Lrect.width)
+        self.croppos = min((mouse[0]+1)/2, self.Lrect.width)
 
-        self.Lrect.left = self.disprect.centerx - crop
+        self.Lrect.left = self.disprect.centerx - self.croppos
         self.Rrect.left = self.disprect.centerx
-        self.Lcrop.width = crop
-        self.Rcrop.width = crop
-        self.Rcrop.left = self.Rrect.width - crop
+        self.Lcrop.width = self.croppos
+        self.Rcrop.width = self.croppos
+        self.Rcrop.left = self.Rrect.width - self.croppos
 
+        if self.rotate90 == True:
+            self.rotateString = '90deg'
+        else:
+            self.rotateString = ''
+        if self.rightSideMirror == True:
+            self.mirrorString = 'Rmirror'
+        else:
+            self.mirrorString = 'Lmirror'
+        if self.verticalFlip == True:
+            self.flipString = 'Vflip'
+        else:
+            self.flipString = ''
 def main():
 
     pygame.init()
@@ -101,22 +136,21 @@ def main():
                 going = False
             elif event.type == KEYDOWN and event.key == K_ESCAPE:
                 going = False
-            elif event.type == KEYDOWN and event.key == K_1:
-                bild.rotate(0)
-                bild.resize(screen)
-            elif event.type == KEYDOWN and event.key == K_2:
-                bild.rotate(90)
-                bild.resize(screen)
-            elif event.type == KEYDOWN and event.key == K_3:
-                bild.rotate(180)
-                bild.resize(screen)
-            elif event.type == KEYDOWN and event.key == K_4:
-                bild.rotate(270)
-                bild.resize(screen)
+            elif event.type == KEYDOWN and event.key == K_d:
+                print pygame.mouse.get_pos()
+                print bild.croppos
+            elif event.type == KEYDOWN and event.key == K_p:
+                save_image(screen, bild.filename + ' ' + str(bild.croppos) + ' ' + bild.mirrorString + ' ' + bild.rotateString + ' ' + bild.rotateString + '.bmp')
             elif event.type == KEYDOWN and event.key == K_q:
-                bild.flip(True)
+                bild.orientation()
+                bild.resize(screen)
+                print 'Roterad: ', bild.rotate90
             elif event.type == KEYDOWN and event.key == K_w:
-                bild.flip(False)
+                bild.flip('horizontal')
+                print 'Högerspegel: ', bild.rightSideMirror
+            elif event.type == KEYDOWN and event.key == K_e:
+                bild.flip('vertical')
+                print 'Uppochner: ', bild.verticalFlip
             elif event.type == KEYDOWN and event.key == K_f:
                 if fullscreen:
                     fullscreen = False
